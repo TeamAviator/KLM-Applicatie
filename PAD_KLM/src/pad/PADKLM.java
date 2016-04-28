974f19ad9a593bf6b3e3672666e8f3991493c996
Florian van Randen <fvanranden@gmail.com>
2016-04-28 13:36:14 +0200

    array list plus jeroens veranderingen

--- PAD_KLM/src/pad/PADKLM.java
+++ PAD_KLM/src/pad/PADKLM.java
@@ -20,7 +20,13 @@ public class PADKLM extends Application {
     
     LoginController loginController = new LoginController();
     
-
+public void cargo(){
+    ArrayList cargoLijst = new ArrayList();
+    Cargo cargo1 = new Cargo(4,"bloemen",20,20,20,"nee");
+    
+    cargoLijst.add(cargo1);
+    
+    }
     
     
     @Override 
