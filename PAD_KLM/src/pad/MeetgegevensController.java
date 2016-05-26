e5f36d61f2c72590bd14774db2c92dd2335ffb1f
Florian van Randen <fvanranden@gmail.com>
2016-05-26 12:59:39 +0200

    meetgegevens

--- PAD_KLM/src/pad/MeetgegevensController.java
+++ PAD_KLM/src/pad/MeetgegevensController.java
@@ -7,11 +7,18 @@ package pad;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -25,11 +32,66 @@ public class MeetgegevensController implements Initializable {
      * Initializes the controller class.
      * @return 
      */
+    @FXML
+    private TextField cargoNummer;
+    @FXML
+    private TextField volume;
+    @FXML
+    private TextField gewicht;
+    @FXML 
+    private TextField aankomstTijd;
+    @FXML 
+    private TextField beschadigd;
+    
+    private int CargoNummer;
+    private int Volume;
+    private int Gewicht;
+    private String DatumTijd;
+    private String Beschadigd;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    /**
+     *
+     * @param event
+     * @throws IOExeption
+     */
+    public void addCargo(ActionEvent event) throws IOException{
+        
+        CargoNummer = Integer.parseInt(cargoNummer.getText());
+        Volume = Integer.parseInt(volume.getText());
+        Gewicht = Integer.parseInt(gewicht.getText());
+        DatumTijd = aankomstTijd.getText();
+        Beschadigd = beschadigd.getText();
+        
+        
+        try (Connection conn = Database.initDatabase()) {
+            conn.setAutoCommit(false);
+        String addCargo 
+                 = "INSERT INTO meetgegevens (vracht_id, gewicht, volume, datum_tijd, beschadigd) "
+                 + "VALUES (?,?,?,?,?) " ;
+        
+        //Create prepared statment
+            PreparedStatement ADDCARGO = conn.prepareStatement(addCargo);
+
+            //set values
+            ADDCARGO.setInt(1, CargoNummer);
+            ADDCARGO.setInt(2, Gewicht);
+            ADDCARGO.setInt(3, Volume);
+            ADDCARGO.setString(4, DatumTijd);
+            ADDCARGO.setString(5, Beschadigd);
+            
+            ADDCARGO.execute();
+            conn.commit();
+        
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CargoLijstController.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
 }
